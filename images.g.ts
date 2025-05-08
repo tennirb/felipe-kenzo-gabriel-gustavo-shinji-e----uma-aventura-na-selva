f0f28917e78450b22c63d2de490a4ab0398a0b0f
tennirb <gabriel.castro@pioneiro.g12.br>
2025-05-08 10:50:14 -0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f . . . . . . . 
+. . . f f f f f f f f . . . . . 
+. . . f f f f f f f f . . . . . 
+. . . . f e e e e e f . . . . . 
+. . . . f 1 f e f 1 f . . . . . 
+. . . . e 1 f e f 1 e . . . . . 
+. . . . e e e e e e e . . . . . 
+. . . . . . 6 e 8 . . . . . . . 
+. . . . 6 8 9 9 8 6 . . . . . . 
+. . . . e 6 8 8 6 e . . . . . . 
+. . . . e 8 8 8 8 e . . . . . . 
+. . . . f 8 8 8 8 f . . . . . . 
+. . . . . e . . e . . . . . . . 
+. . . . . e . . e . . . . . . . 
+. . . . . f . . f . . . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f f e f f f f f . . . . . 
+. . . f e e e e e e f . . . . . 
+. . . . 1 f e e f 1 f . . . . . 
+. . . . 1 f e e f 1 . . . . . . 
+. . . . e e e e e e . . . . . . 
+. . e e 2 2 2 2 2 2 e e . . . . 
+. . e e 2 2 2 2 2 2 e e . . . . 
+. . e e 2 2 2 2 2 2 e e . . . . 
+. . e e 2 2 2 2 2 2 e e . . . . 
+. . . . 9 9 9 9 9 9 . . . . . . 
+. . . . 9 9 9 9 9 9 . . . . . . 
+. . . . e e . . e e . . . . . . 
+`;
+            case "image4":
+            case "myImage2":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 b b b b 7 7 7 7 7 7 7 
+7 7 7 7 7 b 5 5 b b 7 7 7 7 7 7 
+7 7 7 7 b b 7 7 b b b 7 7 7 7 7 
+7 7 7 b b 5 5 5 5 5 b b 7 7 7 7 
+7 7 7 b b 7 7 7 7 7 b b 7 7 7 7 
+7 7 7 e 5 5 5 5 5 5 5 e 7 7 7 7 
+7 7 e 7 7 7 7 f f 7 7 e e 7 7 7 
+7 e e 5 5 5 5 f f 5 5 5 e e 7 7 
+e e e 7 7 7 7 f f 7 7 7 7 e e 7 
+e e 5 5 5 5 f f f f 5 5 5 e e e 
+e c c c c c c c c c c c c c c e 
+c c c c c c c c c c c c c c c c 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+            case "image2":
+            case "myImage0":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 6 6 6 6 6 7 7 7 7 7 7 7 7 7 
+7 6 6 6 6 7 6 6 6 7 7 7 7 7 7 7 
+7 7 6 7 6 6 6 7 6 6 7 7 7 7 7 7 
+7 7 6 6 6 6 6 6 6 6 6 7 7 7 7 7 
+7 7 7 6 6 6 7 6 6 6 6 7 7 7 7 7 
+7 7 7 7 6 6 6 6 6 6 6 6 7 7 7 7 
+7 7 7 7 7 6 7 6 6 6 6 6 7 7 7 7 
+7 7 7 7 7 7 6 6 6 7 6 6 6 7 7 7 
+7 7 7 7 7 7 7 6 6 6 6 7 6 7 7 7 
+7 7 7 7 7 7 7 7 6 6 6 6 6 7 7 7 
+7 7 7 7 7 7 7 7 7 6 6 6 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,299 @@
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+}
+namespace StatusBarKind {
+    export const Medo = StatusBarKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile32`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`myTile1`)
+    info.changeScoreBy(1)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile33`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`myTile1`)
+    info.changeLifeBy(-1)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile37`, function (sprite, location) {
+    info.setLife(1)
+    tiles.setCurrentTilemap(tilemap`level5`)
+    timer.after(5000, function () {
+        tiles.setCurrentTilemap(tilemap`level4`)
+    })
+    cobra = sprites.create(img`
+        . . . . c c c c c c . . . . . . 
+        . . . c 6 7 2 7 7 6 c . . . . . 
+        . . c 7 7 7 2 7 7 7 7 c . . . . 
+        . c 6 7 7 7 2 7 7 7 7 6 c . . . 
+        . c 7 2 6 2 2 6 2 7 7 7 c . . . 
+        . f 7 6 2 6 6 2 6 7 7 7 f . . . 
+        . f 7 2 7 2 2 7 2 7 2 7 f . . . 
+        . . f 7 7 2 7 6 c 7 2 6 f c . . 
+        . . . f c 2 c c 7 7 2 f 7 2 c . 
+        . . c 2 2 2 7 2 6 c 2 7 7 2 7 c 
+        . c 7 2 2 2 7 2 f c 2 7 7 2 c c 
+        c 2 1 2 1 2 6 2 c c 2 6 2 c . . 
+        f 2 1 2 1 2 6 2 c 6 2 6 2 f . . 
+        f 2 1 2 1 2 1 2 6 6 2 6 2 f . . 
+        . 2 6 2 1 2 1 2 1 6 2 6 2 . . . 
+        . 2 c c c c c c c c c f 2 . . . 
+        `, SpriteKind.NPC)
+    story.printCharacterText("Fernando matou uma cobra que tentou atacá-lo. Ele comeu choalete e bebeu água, agora está indo pela direção oposta do Sol para reencontrar o acampamento.", "Narrador")
+})
+info.onLifeZero(function () {
+    timer.after(500, function () {
+        story.startCutscene(function () {
+            story.printCharacterText("Fernando está com muito medo de onças, para dormir ele fez uma rede de folhas d=e gravetos, amarrando-se para não cair.", "Narrador")
+            Notification.notify("Encoste na rede para dormir...", 1, assets.image`myImage1`)
+        })
+        tiles.setCurrentTilemap(tilemap`bag`)
+    })
+    Fernando.setPosition(70, 62)
+})
+info.onScore(5, function () {
+    Notification.waitForNotificationFinish()
+    timer.after(500, function () {
+        Notification.notify("Onde eu estou? onde está o Toninho? EU ME PERDi!", 0.5, img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        Notification.waitForNotificationFinish()
+        Notification.notify("Colete 6 folhas para fazer uma rede paa dormir nela.", 0.5, img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        Notification.waitForNotificationFinish()
+        tiles.setCurrentTilemap(tilemap`level0`)
+    })
+    timer.after(500, function () {
+    	
+    })
+    Notification.waitForNotificationFinish()
+    info.setScore(0)
+})
+let cobra: Sprite = null
+let Fernando: Sprite = null
+let Toninho = sprites.create(assets.image`myImage`, SpriteKind.NPC)
+Fernando = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+info.setScore(0)
+Toninho.setPosition(34, 30)
+controller.moveSprite(Fernando, 100, 100)
+tiles.setCurrentTilemap(tilemap`level`)
+scene.cameraFollowSprite(Fernando)
+scaling.scaleToPixels(Toninho, 20, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+story.cancelSpriteMovement(Fernando)
+story.setPagePauseLength(200, 1000)
+story.printCharacterText("Fernando tenta construir uma cabana no quintal de sua casa, ele não sabia que ia chover no dia que dormiu dentro da cabana e teve uma gripe forte. Seu pai achou melhor colocá-lo no grupo de escoteiros com seu irmão Toninho.", "Narrador")
+timer.after(22000, function () {
+    Notification.notify("O fernando, pode pegar 5 gravetos para mim? Por favor.", 1, assets.image`myImage`)
+    Notification.waitForNotificationFinish()
+    Notification.notify("Vou pegar lá!")
+    tiles.setCurrentTilemap(tilemap`level1`)
+    timer.after(1, function () {
+        sprites.destroy(Toninho)
+    })
+})
+spriteutils.setLifeImage(assets.image`myImage0`)
+info.setLife(6)
+forever(function () {
+    characterAnimations.loopFrames(
+    Fernando,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . f e e . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . 9 9 6 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . f e f e . . . . . . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingLeft)
+    )
+})
+forever(function () {
+    characterAnimations.loopFrames(
+    Fernando,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f f f f f f f f . . . . . 
+        . . . f f f f f f f f . . . . . 
+        . . . . f e e e e e f . . . . . 
+        . . . . f 1 f e f 1 f . . . . . 
+        . . . . e 1 f e f 1 e . . . . . 
+        . . . . e e e e e e e . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . e e 9 9 9 6 e . . . . . 
+        . . . e e 9 9 9 6 e e . . . . . 
+        . . e e . 9 9 9 9 e e e . . . . 
+        . . . . . 9 9 9 9 e e e . . . . 
+        . . . . . d d d d d . . . . . . 
+        . . . . . d d . d d . . . . . . 
+        . . . . . e e . e e . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingDown)
+    )
+})
+forever(function () {
+    characterAnimations.loopFrames(
+    Fernando,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e f . . . . . . . 
+        . . . . . . e e e . . . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . 9 9 6 . . . . . . . 
+        . . . . . . 9 9 9 . . . . . . . 
+        . . . . . . d d d . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . . e . e . . . . . . . 
+        . . . . . . e f e f . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingRight)
+    )
+})
+forever(function () {
+    characterAnimations.loopFrames(
+    Fernando,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . e e f f f e e . . . . . 
+        . . . . e e e e e e e . . . . . 
+        . . . . . . 6 e 8 . . . . . . . 
+        . . . . e e 9 9 9 6 e . . . . . 
+        . . . e e 9 9 9 6 e e . . . . . 
+        . . e e . 9 9 9 9 e e e . . . . 
+        . . . . . 9 9 9 9 e e e . . . . 
+        . . . . . d d d d d . . . . . . 
+        . . . . . d d . d d . . . . . . 
+        . . . . . e e . e e . . . . . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+})
+forever(function () {
+    characterAnimations.loopFrames(
+    cobra,
+    [img`
+        . . . . c c c c c c . . . . . . 
+        . . . c 6 7 7 7 7 6 c . . . . . 
+        . . c 7 7 7 7 7 7 7 7 c . . . . 
+        . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+        . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+        . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+        . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+        . . f 7 7 7 7 6 c 7 7 6 f c . . 
+        . . . f c c c c 7 7 6 f 7 7 c . 
+        . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+        . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+        c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+        f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+        . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+        . . c c c c c c c c c f . . . . 
+        `,img`
+        . . . . c c c c c c . . . . . . 
+        . . . c 6 7 7 7 7 6 c . . . . . 
+        . . c 7 7 7 7 7 7 7 7 c . . . . 
+        . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+        . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+        . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+        . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+        . . f 7 7 7 7 6 c 7 7 6 f c . . 
+        . . . f c c c c 7 7 6 f 7 7 c . 
+        . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+        . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+        c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+        f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+        f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+        . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+        . . c c c c c c c c c f . . . . 
+        `,img`
+        . . . c c c c c c . . . . . . . 
+        . . c 6 7 7 7 7 6 c . . . . . . 
+        . c 7 7 7 7 7 7 7 7 c . . . . . 
+        c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+        c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+        f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+        f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+        . f 7 7 7 7 6 c 7 7 6 f . . . . 
+        . . f c c c c 7 7 6 f c c c . . 
+        . . c 6 2 7 7 7 f c c 7 7 7 c . 
+        . c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+        . c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+        . c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+        . . c 6 1 1 1 1 1 7 6 6 c c . . 
+        . . . c c c c c c c c c c . . . 
+        `,img`
+        . . . c c c c c c . . . . . . . 
+        . . c 6 7 7 7 7 6 c . . . . . . 
+        . c 7 7 7 7 7 7 7 7 c . . . . . 
+        c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+        c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+        f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+        f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+        . f 7 7 7 7 6 c 7 7 6 f . . . . 
+        . . f c c c c 7 7 6 f c c c . . 
+        . . c 6 2 7 7 7 f c c 7 7 7 c . 
+        . c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+        . c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+        . c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+        . c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+        . . c 6 1 1 1 1 1 7 6 6 c c . . 
+        . . . c c c c c c c c c c . . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.NotMoving)
+    )
+})
